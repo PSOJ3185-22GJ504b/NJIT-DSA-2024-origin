5884e3ea01025ea8cea9880abe97ae7ca3c3d6a8
zhangzenan <1615468396@qq.com>
2024-04-02 16:05:34 +0800

    4.2

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -49,6 +49,34 @@ public class Algorithms {
          return fromIndex;  
       }  
       return -1;  
-   }  
+   }
 
+   public static <T extends Comparable<T>> void quickSort(T[] array) {
+      quickSort(array, 0, array.length - 1);
+   }
+
+   private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high) {
+      if (low < high) {
+         int pi = partition(array, low, high);
+         quickSort(array, low, pi - 1);
+         quickSort(array, pi + 1, high);
+      }
+   }
+
+   private static <T extends Comparable<T>> int partition(T[] array, int low, int high) {
+      T pivot = array[high];
+      int i = low - 1;
+      for (int j = low; j < high; j++) {
+         if (array[j].compareTo(pivot) < 0) {
+            i++;
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+         }
+      }
+      T temp = array[i + 1];
+      array[i + 1] = array[high];
+      array[high] = temp;
+      return i + 1;
+   }
 }
