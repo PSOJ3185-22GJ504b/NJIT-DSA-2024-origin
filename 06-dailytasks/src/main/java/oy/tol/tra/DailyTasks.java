bf34d3bd74c78d2ac3ddd1a92c6aa4137e3bb0c3
zhangzenan <1615468396@qq.com>
2024-04-12 12:29:00 +0800

    4.12

--- 06-dailytasks/src/main/java/oy/tol/tra/DailyTasks.java
+++ 06-dailytasks/src/main/java/oy/tol/tra/DailyTasks.java
@@ -3,10 +3,6 @@ package oy.tol.tra;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.nio.file.Files;  
-import java.nio.file.Paths;  
-import java.util.LinkedList;  
-import java.util.Queue; 
 
 /**
  * A class showing your daily schedule using a timer.
@@ -33,27 +29,30 @@ public class DailyTasks {
       try {
          // TODO:
          // 1. create a queue (to the member variable!) for daily tasks, which are strings.
-         dailyTaskQueue=new QueueImplementation<>();
+         dailyTaskQueue = new QueueImplementation<>();
          // 2. read the tasks for today by calling readTasks() -- implementing missing parts of it!
-         readTasks();  
+         readTasks();
          // 3. create Java Timer object (to member variable) to schedule your daily tasks. (Already given to you.)
          timer = new Timer();
          // 4. schedule the timer at fixed rate with a new TimerTask,
          //  using the delay constant values in the class member variable. (Already given to you.)
-         timer.scheduleAtFixedRate(new TimerTask() {
+         timer.scheduleAtFixedRate(new  TimerTask() {
             // 4.1 in the timer task run:
             @Override
             public void run() {
-               // 4.1.1 check if there are tasks in the queue:
-               if (!taskQueue.isEmpty()) { 
+                  // 4.1.1 check if there are tasks in the queue:
                   // 4.1.2 if yes, print the task from the queue, dequeueing it.
-                    String task = taskQueue.poll();  
-                        System.out.println("Executing task: " + task);  
-                    } else {  
                   // 4.1.3 if not, cancel the timer.
-                  timer.cancel();  
-                  System.out.println("No more tasks to execute. Timer canceled.");  
-              }  
+               if (!dailyTaskQueue.isEmpty()) {
+                  try {
+                     String task = dailyTaskQueue.dequeue();
+                     System.out.println(task);
+                  } catch (QueueIsEmptyException e) {
+                     System.out.println("No more tasks to perform.");
+                  }
+               } else {
+                  timer.cancel();
+               }
             }
          }, TASK_DELAY_IN_SECONDS, TASK_DELAY_IN_SECONDS);
       } catch (IOException e) {
@@ -66,10 +65,13 @@ public class DailyTasks {
       tasks = new String(getClass().getClassLoader().getResourceAsStream("DailyTasks.txt").readAllBytes());
       String[] allTasks = tasks.split("\\r?\\n");
       for (String task : allTasks) {
-         // TODO: Enqueue the task to your Queue implementation:
-         taskQueue.add(task);  
+         try{
+            dailyTaskQueue.enqueue(task);
+         }catch (QueueAllocationException | NullPointerException e){
+            System.out.println(e.getMessage());
+         }
       }
       // TODO: print out to the console the number of tasks in the queue:
-      System.out.println("Number of tasks in queue: " + taskQueue.size());  
+      System.out.println("Number of tasks in the queue: " + dailyTaskQueue.size());
    }
 }
